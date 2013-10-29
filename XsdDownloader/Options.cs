9634ee6173f69cfc1e6ec02a6ae06c3e3fccde04
Martins Osis <martins.osis@fms.lv>
2013-10-29 10:41:25 +0200

    Initial commit.

---
+++ XsdDownloader/Options.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace XsdDownloader {
+    class Options {
+        [OptionList('i', "input", ';', Required = true, HelpText = "URL of the root XSD file(s).")]
+        public IList<String> InputFiles { get; set; }
+
+        [Option('o', "output", HelpText = "Directory where to save all downloaded files.")]
+        public String OutputDirectory { get; set; }
+
+        [Option('n', "namespace", HelpText = "Namespace for the generated class(es).")]
+        public String Namespace { get; set; }
+
+        [HelpOption]
+        public String GetUsage() {
+            return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ XsdDownloader/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XsdDownloader {
+    class Program {
+        public static Options Params = new Options();
+        public static List<String> Processed = new List<String>();
+        public static List<String> Imported = new List<string>();
+        public static List<String> Included = new List<string>();
+
+        static void Main(string[] args)
+        {
+            if (!CommandLine.Parser.Default.ParseArguments(args, Params))
+                return;
+
+            // Download & process XSDs
+            if (String.IsNullOrWhiteSpace(Params.OutputDirectory))
+                Params.OutputDirectory = ".";
+            if (!Directory.Exists(Params.OutputDirectory))
+                Directory.CreateDirectory(Params.OutputDirectory);
+
+            foreach (var inputFile in Params.InputFiles) {
+                DownloadAndParse(inputFile);
+            }
+
+            // Generate batch file for xsd.exe
+            var sb = new StringBuilder("xsd.exe /c");
+
+            foreach (var baseName in Params.InputFiles.Select(Path.GetFileName)) {
+                AddToImported(baseName);
+            }
+
+            if (!String.IsNullOrWhiteSpace(Params.Namespace))
+                sb.Append(" /namespace:").Append(Params.Namespace);
+
+            foreach (var imported in Imported) {
+                sb.Append(" ").Append(imported);
+            }
+
+            File.WriteAllText(Params.OutputDirectory + @"\create_classes_from_xsd.bat", sb.ToString());
+        }
+
+        static void DownloadAndParse(String url)
+        {
+            // Skip already processed XSDs
+            if (Processed.Contains(url)) {
+                return;
+            }
+
+            Console.WriteLine("Downloading and parsing: " + url);
+            // Initialize paths
+            var uri = new Uri(url);
+            var fileName = Params.OutputDirectory + @"\" + Path.GetFileName(uri.LocalPath);
+            var baseUri = uri.AbsoluteUri.Remove(uri.AbsoluteUri.Length - uri.Segments.Last().Length);
+            XDocument doc;
+
+            // Download and parse
+            using (var client = new WebClient()) {
+                client.DownloadFile(url, fileName);
+                doc = XDocument.Load(fileName);
+            }
+
+            // Recursively download all <include>d schemas
+            foreach (var schemaFile in doc.Descendants()
+                                          .Where(e => e.Name.LocalName == "include")
+                                          .Select(e => e.Attribute("schemaLocation").Value)) {
+                DownloadAndParse(baseUri + schemaFile);
+                Included.Add(schemaFile);
+            }
+
+            // Recursively download all <import>ed schemas and put them on the command line (since
+            // xsd.exe expects <import> 
+            foreach (var path in doc.Descendants()
+                                    .Where(e => e.Name.LocalName == "import")
+                                    .Select(e => e.Attribute("schemaLocation").Value)) {
+                AddToImported(path);
+                DownloadAndParse(path);
+            }
+
+            Processed.Add(url);
+        }
+
+        static void AddToImported(String filename)
+        {
+            if (!Imported.Contains(Path.GetFileName(filename)) && !Included.Contains(Path.GetFileName(filename)))
+                Imported.Add(Path.GetFileName(filename));
+        }
+    }
+}
